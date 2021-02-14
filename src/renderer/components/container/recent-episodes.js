71b174c88818196b7a36e9a7b08a69cb989d0fc7
Wayne He <28408948+Ecpii@users.noreply.github.com>
2021-02-14 14:45:39 -0500

    Add scroll buttons, two line title wrap to recent episodes

--- src/renderer/components/container/recent-episodes.js
+++ src/renderer/components/container/recent-episodes.js
@@ -12,6 +12,7 @@ export default class RecentEpisodes extends React.Component {
       updated: false,
       page: 1
     }
+    this.ref = React.createRef();
   }
 
   componentDidMount() {
@@ -24,22 +25,32 @@ export default class RecentEpisodes extends React.Component {
     })
   }
 
+  scroll = (scrollOffset) => {
+    this.ref.current.scrollLeft += scrollOffset // todo - actually scroll to each element instead of a num
+  }
+
   render() {
     return (
       <div className={"no-selection"}>
         <h1 className={"recent-episodes-title"}>Recent</h1>
         <div className={"episode-releases-container"}>
-          {this.state.updated && this.state.recent.map(element => {
-            element = element[1][0];
+          <button className={"scroll-button-left"} onClick={() => this.scroll(-170)}>←</button>
 
-            return (
-              <EpisodeCard
-                anime_name={element.name}
-                episode_number={element.episode}
-                link={element.link}
-                history={this.props.history}
-              />)
-          })}
+          <div className={"episode-releases"} ref={this.ref}>
+            {this.state.updated && this.state.recent.map(element => {
+              element = element[1][0];
+
+              return (
+                <EpisodeCard
+                  anime_name={element.name}
+                  episode_number={element.episode}
+                  link={element.link}
+                  history={this.props.history}
+                />)
+            })}
+          </div>
+
+          <button className={"scroll-button-right"} onClick={() => this.scroll(170)}>→</button>
         </div>
       </div>
     )
