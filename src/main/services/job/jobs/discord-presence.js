ae60bdd25a4245f527ea1ebf597f01e18d8fbedd
Aqil Contractor <aqilcm@gmail.com>
2021-04-05 18:19:03 -0400

    ability to turn off discord RPC :D

--- src/main/services/job/jobs/discord-presence.js
+++ src/main/services/job/jobs/discord-presence.js
@@ -24,20 +24,14 @@ export default class DiscordPresence extends Job {
   async run() {
     RPC.register(CLIENT_ID);
 
-    const store = global.store;
-
-    let prevPresenceData;
-
-    store.subscribe(() => {
-      let data = store.getState()['current-presence'], state = data.state;
+    const store = global.store, subfun = () => {
+      let data = store.getState()['current-presence'], { state } = data;
 
       if (data && !_.isEqual(prevPresenceData, data)) {
         let statusText = PresenceConstants.STATUS_TO_STATE[state];
         switch (state) {
           case PresenceState.WATCHING_ANIME:
             statusText = `${data.player.anime.title} Episode ${data.player.anime.episode}`;
-            break;
-          default:
             break;
         }
 
@@ -56,28 +50,43 @@ export default class DiscordPresence extends Job {
           if (data.endTimestamp) activity.endTimestamp = data.endTimestamp;
         }
 
-        if (activity) client.setActivity(activity)
+        client.setActivity(activity)
 
         prevPresenceData = data;
       }
+    };
+
+    let prevPresenceData, unsub = store.subscribe(subfun), lastval = true;
+
+    global.events.on('rpc', val => {
+      if(val === lastval) return;
+      lastval = val;
+      if(val) { console.log("rpc should be on"); unsub = store.subscribe(subfun); connectv2(); }
+      else { console.log("unrpc"); unsub(); client.destroy(); }
     })
 
-    const client = new RPC.Client({ transport: 'ipc' });
+    let client;
+    const connectv2 = async function() {
+      client = new RPC.Client({ transport: 'ipc' });
+      client.on('ready', () => {
+        console.log("ready");
+        let startTimestamp = new Date();
+        client.setActivity({
+          state: PresenceConstants.STATUS_TO_STATE[PresenceState.IDLE],
+          startTimestamp,
+          largeImageKey: PresenceConstants.IMAGE_KEY,
+          largeImageText: PresenceConstants.LARGE_TEXT,
+          smallImageText: PresenceConstants.SMALL_TEXT,
+          instance: false,
+        })
+      });
 
-    client.on('ready', () => {
-      let startTimestamp = new Date();
-      client.setActivity({
-        state: PresenceConstants.STATUS_TO_STATE[PresenceState.IDLE],
-        startTimestamp,
-        largeImageKey: PresenceConstants.IMAGE_KEY,
-        largeImageText: PresenceConstants.LARGE_TEXT,
-        smallImageText: PresenceConstants.SMALL_TEXT,
-        instance: false,
-      })
-    });
-
-    client.login({
-      clientId: CLIENT_ID
-    });
+      try {
+        await client.login({
+          clientId: CLIENT_ID
+        });
+      } catch(err) { console.log(err); }
+    }
+    connectv2();
   }
 }
